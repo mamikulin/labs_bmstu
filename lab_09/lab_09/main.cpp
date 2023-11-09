58110f61dacc0e2ab73de97b11a0f971cdfcc02d
mamikulin <mamikulin@gmail.com>
2023-11-09 23:17:00 +0300

    tasks

--- lab_09/lab_09/main.cpp
+++ lab_09/lab_09/main.cpp
@@ -5,10 +5,6 @@
 //  Created by Mike Mikulin on 09.11.2023.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    
 }
